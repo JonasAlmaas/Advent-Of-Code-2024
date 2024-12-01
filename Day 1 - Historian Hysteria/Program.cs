8aa7eee272f0e3d2cab1b35312b04537da089dad
Jonas Almaas <jonas.f.almaas@gmail.com>
2024-12-01 17:01:05 +0100

    Day 1 part 1

---
+++ Day 1 - Historian Hysteria/Program.cs
@@ -0,0 +1,19 @@
+﻿var fName = "input.txt";
+// var fName = "example.txt";
+
+var (list1, list2) = File.ReadAllLines(fName)
+	.Select(l => l.Split("   "))
+	.Select(split => (int.Parse(split[0]), int.Parse(split[1])))
+	.Aggregate(
+		(l1: new List<int>(), l2: new List<int>()),
+		(acc, pair) => {
+			acc.l1.Add(pair.Item1);
+			acc.l2.Add(pair.Item2);
+			return acc;
+		});
+
+var result = list1.Order()
+	.Zip(list2.Order(), (a, b) => Math.Abs(a - b))
+	.Sum();
+
+Console.WriteLine(result);
